e79305ec4809f0fd42a00160a012024c3527a5a6
Kevin Lindsey <kevin@kevlindev.com>
2018-11-09 15:11:51 -0800

    Read data from 'last_sample' file

--- app/controllers/sensor_controller.js
+++ app/controllers/sensor_controller.js
@@ -1,20 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const last_sample_file = path.join(__dirname, "..", "db", "last_sample.json");
+
+
 function round(number, decimal_digits) {
     const factor = Math.pow(10, decimal_digits);
 
     return Math.round(number * factor) / factor;
 }
 
+function read_last_sample() {
+    let result = null;
+
+    if (fs.existsSync(last_sample_file)) {
+        const text = fs.readFileSync(last_sample_file);
+        console.log(`text = ${text}`);
+
+        try {
+            result = JSON.parse(text);
+        }
+        catch(e) {
+        }
+    }
+
+    return result;
+}
+
 function get_sensor_data(req, res) {
     const precision = 2;
-    const temperature = 27 + Math.random();
-    const ec = 200 + Math.random();
-    const ph = 10 + Math.random();
+    const data = read_last_sample();
 
-    res.render("sensors", {
-        temperature: round(temperature, precision),
-        ec: round(ec, precision),
-        ph: round(ph, precision)
-    });
+    if (data !== null) {
+        res.render("sensors", {
+            temperature: round(data.temperature, precision),
+            ec: round(data.ec, precision),
+            ph: round(data.pH, precision)
+        });
+    }
+    else {
+        res.render("sensors", {});
+    }
 }
 
 
