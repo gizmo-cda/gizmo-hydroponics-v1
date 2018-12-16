d1931069da7ac1c9ee421903bf85e7936a41e4d0
Kevin Lindsey <kevin@kevlindev.com>
2018-12-15 17:41:58 -0800

    Set initial state of light and pump buttons

--- app/controllers/sensor_controller.js
+++ app/controllers/sensor_controller.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const async = require('async');
+const request = require('request');
 const last_sample_file = path.join(__dirname, "..", "db", "last_sample.json");
 
 
 function round(number, decimal_digits) {
+    if (number === undefined) {
+        return undefined;
+    }
+
     const factor = Math.pow(10, decimal_digits);
 
     return Math.round(number * factor) / factor;
@@ -26,20 +32,49 @@ function read_last_sample() {
     return result;
 }
 
+function send_command(command, callback) {
+    request.get(
+        {
+            url: `http://localhost:8081/${command.toLowerCase()}`
+        },
+        (err, response, body) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            else {
+                callback(null, JSON.parse(body));
+            }
+        }
+    )
+}
+
 function get_sensor_data(req, res) {
     const precision = 2;
-    const data = read_last_sample();
 
-    if (data !== null) {
-        res.render("sensors", {
-            temperature: round(data.temperature, precision),
-            ec: round(data.ec, precision),
-            ph: round(data.pH, precision)
-        });
-    }
-    else {
-        res.render("sensors", {});
-    }
+    async.series(
+        {
+            light: next => send_command('read_light', next),
+            motor: next => send_command('read_motor', next)
+        },
+        (err, results) => {
+            if (err) {
+                console.error('An error occurred getting sensor data: ' + err);
+                res.render("sensors", {});
+            }
+            else {
+                const data = read_last_sample() || {};
+
+                res.render("sensors", {
+                    temperature: round(data.temperature, precision),
+                    ec: round(data.ec, precision),
+                    ph: round(data.pH, precision),
+                    light: results.light.value ? "true" : "false",
+                    motor: results.motor.value ? "true" : "false"
+                });
+            }
+        }
+    );
 }
 
 function get_schedule(req, res) {
